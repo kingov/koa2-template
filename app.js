75b5f2e01578d50954d0e6bfd1c4bdc99eca321b
wangzheng <waangzheeng@gmail.com>
2019-01-02 16:19:57 +0800

    修改错误

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use((ctx, next) => {
 
 app.on('error', (err, ctx, c) => {
   console.error('server error', err);
-  // console.error('server error ctx', ctx);
+  console.error('server error ctx', ctx);
   // console.error('server error c', c);
   // ctx.render('error')
 });
