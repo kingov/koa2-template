2042b9d4a89b927608b95b5a64d1d472ce871e8e
wangzheng <waangzheeng@gmail.com>
2019-01-02 17:08:13 +0800

    下载文件

--- route/apiRoute.js
+++ route/apiRoute.js
@@ -17,7 +17,8 @@ route.get('/get-data', async(ctx)=>{
 
 // 下载文件
 route.get('/get-file', (ctx) => {
-  ctx.infolog('get-file   下载文件')
+  ctx.infolog('get-file下载路径',  __dirname + '/../public/')
+
   send(ctx, 'img/xm2.jpeg', { root: __dirname + '/../public/' })
 })
 
